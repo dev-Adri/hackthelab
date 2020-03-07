777553412ea406207fc3627f174031a0ff1c357f
Adri <46003311+dev-Adri@users.noreply.github.com>
2020-03-07 19:02:06 +0200

    Update

--- javascript/board.js
+++ javascript/board.js
@@ -2,8 +2,8 @@ window.addEventListener('load', () => {
     const canvas = document.querySelector("#canvas");
     const ctx = canvas.getContext('2d');
 
-    canvas.width = 500;
-    canvas.height = 500;
+    canvas.width = window.innerWidth * 0.75;
+    canvas.height = window.innerHeight * 0.6;
 
 
     var painting = false;
